bb3529d66acba44033f8fa16f3c9934ee2e1e3bb
Lakshay Arora <lakshay16199@gmail.com>
2023-07-17 03:40:18 +0530

    navbar and social icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Navbar from '@/sectons/Navbar'
+import Socialicons from '@/components/Socialicons'
 
 export default function Home() {
   return (
     <main className='app'>
       <Navbar />
+      <Socialicons />
     </main>
   )
 }
--- src/sectons/Navbar.js
+++ src/sectons/Navbar.js
@@ -1,19 +1,43 @@
 import React from 'react'
 import Link from "next/link"
 import Logo from '@/components/Logo'
+import Button from '@/components/Button';
 
 function Navbar() {
-  return (
-    <nav>
-        <div className='wrapper'>
-            <div className="brand">
-                <Link href="">
-                    <Logo />
-                </Link>
+    const sectionLinks = [
+        { name: "About", link: "/#about" },
+        { name: "Skills", link: "/#skills" },
+        { name: "Experience", link: "/#experience" },
+        { name: "Work", link: "/#work" },
+        { name: "Contact", link: "/#contact" },
+    ];
+    return (
+        <nav>
+            <div className='wrapper'>
+
+                <div className="logo">
+                    <Link href="">
+                        <Logo />
+                    </Link>
+                </div>
+
+                <div className="nav-items">
+                    <ul className="nav-items-list">
+                        {sectionLinks.map(({name, link}) => (
+                            <li key={name} className='nav-items-list-item'>
+                                <Link href={link} className='nav-items-list-item-link'>
+                                    {name}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className="nav-items-button">
+                        <Button  text="Resume" link="http://localhost:3000/resume.pdf" />
+                    </div>
+                </div>
             </div>
-        </div>
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
